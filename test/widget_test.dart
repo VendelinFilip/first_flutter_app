06de3b072f3636a6e75d95b5f33f18aa92f071df
VendelinFilip <vendelinfilip73@gmail.com>
2020-10-17 23:28:20 +0200

    Github TEST 2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:first_flutter_app/main.dart';
 
-//MyApp
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
